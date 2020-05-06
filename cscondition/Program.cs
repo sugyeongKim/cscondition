6ed8a9ea4eb19ba89084b88ed465e1738210c957
sugyeong Kim <s2018s25@e-mirim.hs.kr>
2020-05-06 14:51:31 +0900

    13-2 if else 조건문 실습

--- cscondition/Program.cs
+++ cscondition/Program.cs
@@ -13,15 +13,36 @@ namespace cscondition
             Console.Write("숫자 입력: ");
             var input = int.Parse(Console.ReadLine());
 
-            if(input % 2 == 0)
+            if (input % 2 == 0)
             {
                 Console.WriteLine("짝수입니다.");
             }
 
-            if(input % 2 == 1)
+            else
             {
                 Console.WriteLine("홀수입니다.");
             }
+
+
+            //현재시간 구하기
+            Console.WriteLine(DateTime.Now.Year);
+            Console.WriteLine(DateTime.Now.Month);
+            Console.WriteLine(DateTime.Now.Day);
+            Console.WriteLine(DateTime.Now.Hour);
+            Console.WriteLine(DateTime.Now.Minute);
+            Console.WriteLine(DateTime.Now.Second);
+
+
+            //오전과 오후 구분
+            if(DateTime.Now.Hour < 12)
+            {
+                Console.WriteLine("오전입니당");
+            }else
+            {
+                Console.WriteLine("오후입니당");
+            }
+            
+
         }
     }
 }
