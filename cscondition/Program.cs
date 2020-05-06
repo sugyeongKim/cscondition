b19ec7d266bddad945d5fff2387aa9dcfae5e676
sugyeong Kim <s2018s25@e-mirim.hs.kr>
2020-05-06 15:16:23 +0900

    #3 13-5 switch 조건문 실습

--- cscondition/Program.cs
+++ cscondition/Program.cs
@@ -13,7 +13,7 @@ namespace cscondition
             Console.Write("숫자 입력: ");
             var input = int.Parse(Console.ReadLine());
 
-            if (input % 2 == 0)
+           /* if (input % 2 == 0)
             {
                 Console.WriteLine("짝수입니다.");
             }
@@ -21,6 +21,17 @@ namespace cscondition
             else
             {
                 Console.WriteLine("홀수입니다.");
+            }
+            */
+
+            switch(input % 2)
+            {
+                case 0:
+                    Console.WriteLine("짝수입니다.");
+                    break;
+                case 1:
+                    Console.WriteLine("홀수입니다.");
+                    break;
             }
 
 
@@ -45,8 +56,39 @@ namespace cscondition
             {
                 Console.WriteLine("저녁 먹을 시간");
             }
-                
-            
+
+
+            Console.Write("이번 달은 몇 월 인가요?");
+            int input2 = int.Parse(Console.ReadLine());
+
+            switch (input2)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("겨울입니다");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("봄입니다");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("여름입니다");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("가을입니다");
+                    break;
+
+                default:
+                    Console.WriteLine("어디 사세요;;??");
+                    break;
+            }
+
 
         }
     }
